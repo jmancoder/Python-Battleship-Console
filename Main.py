30deea3eec7f0d8ca28513659b5db6043d2879ca
jmancoder <jmancoder2000@gmail.com>
2023-01-04 20:40:09 -0800

    Added the ability to place multiple ships, improved AI, and corrected async functions.

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from asyncio import sleep
+import asyncio
 import random
 
 player_ships = []
@@ -7,49 +7,77 @@ player_shots = []
 ai_shots = []
 shots_fired = []
 
-def validated_player_input():
+def validated_player_input(placing = False):
     """
     Ensures that input coordinates are valid integers,
-     are within the list boundaries, and have not been entered before.
+    are within the list boundaries, and have not been entered before.
+    The 'placing' parameter specifies if the coordinates are used for placing a ship.
     """
     while True:
         try:
-            # Input _s with an offset to account for zero-based indexing
+            # Input coordinates with an offset to account for zero-based indexing
             x = int(input("X: "))-1
             y = int(input("Y: "))-1
 
             # Must be within list boundaries
-            assert (x < 5 and x >= 0) and (y < 5 and y >= 0)
-            # Must not have been entered before
-            assert not player_shots[x][y]
-        except TypeError:
-            print("Those coordinates are invalid. Try again!")
+            if(x >= 5 or x < 0) or (y >= 5 or y < 0):
+                raise ValueError
+            # Must not have been shot/placed before
+            if placing:
+                assert player_ships[x][y] == 0
+            else:
+                assert player_shots[x][y] == 0
         except AssertionError:
             print("You have already entered those coordinates. Try again!")
+        except ValueError:
+            print("Coordinates must be within the 5x5 grid. Try again!")
+        except TypeError:
+            print("Those coordinates are invalid. Try again!")
         else:
             # Stop looping if coordinates are valid
             break
     return [x, y]
 
-def draw_visual_grid(target_list):
+def check_game_over(ship_list):
+    """Checks if a list of ships only contains zeroes,
+    which would mean that all of them are sunk.
+    """
+    for sublist in ship_list:
+        for i in sublist:
+            if i != 0:
+                return False
+    return True
+
+def draw_visual_grid(hits_list):
     """
     Draws a grid in the console that displays all hits,
     misses, and blanks within the input list.
     """
-    for y in range(5):
+    for x in range(0, 5):
         row_symbols = ''
-        for x in range(5):
+        for y in range(0, 5):
             # Print 'X' for hits, 'M' for misses, and '-' for everything else
-            if target_list[x][y] == 2:
+            if hits_list[x][y] == 2:
                 row_symbols += 'X'
-            elif target_list[x][y] == 1:
+            elif hits_list[x][y] == 1:
                 row_symbols += 'M'
             else:
                 row_symbols += '-'
         # Print each row of symbols
         print(row_symbols)
 
-def main():
+def get_random_coordinates(excluded_list):
+    """Generates random AI coordinates to be used for placing ships and firing.
+    Like validated_player_input(), the function loops until it can find valid coordinates.
+    """
+    while True:
+        x = random.randint(0, 4)
+        y = random.randint(0, 4)
+        if not excluded_list[x][y]:
+            excluded_list.append((x, y))
+            return [x, y]
+
+async def main():
     # Create global variables
     global player_ships
     global ai_ships
@@ -59,27 +87,31 @@ def main():
 
     # Initialize global variables
     shots_fired = 0
-    for i in range(5):
-        # TODO: Optimize system so that it requires fewer multi-dimensional lists.
-        player_ships = [[False for j in range(5)] for j in range(5)]
-        ai_ships = [[False for j in range(5)] for j in range(5)]
-        _s = [[0 for j in range(5)] for j in range(5)]
-        ai_shots = [[0 for j in range(5)] for j in range(5)]
+
+    # Store 2D lists of ship IDs
+    player_ships = [[0 for i in range(5)] for j in range(5)]
+    ai_ships = [[0 for i in range(5)] for j in range(5)]
+
+    # Store a list of integers with 0 being blank, 1 being a miss, and 2 being a hit
+    player_shots = [[0 for j in range(5)] for j in range(5)]
+    ai_shots = [[0 for j in range(5)] for j in range(5)]
 
     print("Welcome to 2-player Battleships!")
 
-    # Store validated player ship coordinates
-    # TODO: Add multiple ships with varying dimensions.
-    print("Player 1 enter coordinates for your ship.")
-    player_ship = validated_player_input()
-    player_ships[player_ship[0]][player_ship[1]] = True
+    # Store three sets of validated ship coordinates
+    for i in range(1, 4):
+        print(f"Player 1 enter coordinates for ship {i}.")
+        ship_coords = validated_player_input(True)
+        player_ships[ship_coords[0]][ship_coords[1]] = i
 
-    # Generate random ai ship coordinates
-    sleep(0.5)
-    print("Player 2 entered coordinates for their ship.")
-    ai_ships[random.randint(0, 4)][random.randint(0, 4)] = True
+    # Generate three sets of random ai ship coordinates
+    for j in range(1, 4):
+        ship_coords = get_random_coordinates(ai_ships)
+        ai_ships[ship_coords[0]][ship_coords[1]] = j
 
+    print("Player 2 entered coordinates for their ships.")
     print("Game is starting...")
+    await asyncio.sleep(0.5)
 
     # Execute main game loop.
     while True:
@@ -91,42 +123,52 @@ def main():
         player_shot = validated_player_input()
 
         # Check if the player hit a ship and draw a visual grid of previous shots
-        if ai_ships[_s[0]][_s[1]]:
+        ship_id = ai_ships[player_shot[0]][player_shot[1]]
+        if ship_id > 0:
             player_shots[player_shot[0]][player_shot[1]] = 2
             draw_visual_grid(player_shots)
-            print("Hit! You have sunk player 2's ship.")
-            # TODO: Determine if all of the ai ships have sunk.
-            print("Player 1 wins!")
-            break
+            print("Hit!")
+            if ship_id not in ai_ships:
+                # If the ship sunk, then invalidate its id in the ship list
+                ai_ships[player_shot[0]][player_shot[1]] = 0
+                print("Player 1 sunk a ship!")
+            if check_game_over(ai_ships):
+                # End game if list contains only ship id 0 (all ships sunk)
+                print("Player 1 wins!")
+                break
         else:
             player_shots[player_shot[0]][player_shot[1]] = 1
             draw_visual_grid(player_shots)
             print("Miss... switching players.")
+        
+        await asyncio.sleep(0.5)
 
         # Increase total shot count
         shots_fired += 1
 
-        # Generate random ai attempt coordinates
-        sleep(1)
-        ai_shot = [random.randint(0, 4), random.randint(0, 4)]
+        # Generate random ai shot coordinates
+        ai_shot = get_random_coordinates(ai_shots)
 
         # Check if the ai hit a ship and draw a visual grid of previous shots
-        if player_ships[ai_shot[0]][ai_shot[1]]:
+        ship_id = player_ships[ai_shot[0]][ai_shot[1]]
+        if ship_id > 0:
             ai_shots[ai_shot[0]][ai_shot[1]] = 2
             draw_visual_grid(ai_shots)
-            print("Hit! Player 2 has sunk your ship.")
-            # TODO: Determine if all of the player ships have sunk.
-            print("Player 2 wins!")
-            break
+            if ship_id not in player_ships:
+                # If the ship sunk, then invalidate its ID in the ship list
+                player_ships[ai_shot[0]][ai_shot[1]] = 0
+                print("Player 2 sunk your ship!")
+            if check_game_over(player_ships):
+                # End game if list contains only ship id 0 (all ships sunk)
+                print("Player 2 wins!")
+                break
         else:
-            ai_shots[ai_shot[0]][ai_shot[1]] = 1
-            draw_visual_grid(ai_shots)
             print("Player 2 missed... switching players.")
 
-    # Executes when the game ends
+    # When the game ends, print total shots and delay before closing terminal
     print("Total shots fired:", shots_fired)
-    sleep(2)
+    await asyncio.sleep(2)
 
 # Execute main() function
 if __name__=="__main__":
-    main()
+    asyncio.run(main())
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A simple implementation of battleship that runs in the Python console. I created this as an academic project, but any feedback is welcome.
 
 ## Requirements:
-Python 3.0 or later must be installed to run this program. You can download the latest version of Python from https://www.python.org/. After you have installed it, download this repository and unzip it. Finally, double click on the Main.py file to play battleship in the Python console.
+Python 3.0 or later must be installed to run this program. You can download the latest version of Python from https://www.python.org/. After you have installed it, download this repository and unzip it. Finally, double click on the Main.py file to play battleship.
 
 ## How to play:
 - Enter the X and Y coordinates where you want to place your ship. These must be between 1 and 5.
@@ -10,11 +10,6 @@ Python 3.0 or later must be installed to run this program. You can download the 
 - If you or the AI hit a ship, then the winner will be announced and the console will close.
 
 ## Features
-- The ability to place one ship anywhere on a 5x5 grid.
-- An AI that randomly places a ship and guesses your coordinates.
+- The ability to place three single-coordinate ships anywhere on a 5x5 grid.
+- An AI that randomly places its ships and guesses your coordinates.
 - A visual grid in the console that displays hits and misses each turn
-
-## Planned features:
-- Multiple ships of varying lengths
-- An option to play local multiplayer
-- A customizable difficulty option for the AI
